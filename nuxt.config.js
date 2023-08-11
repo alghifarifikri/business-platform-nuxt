690736ce6af043f26c533b2708b05803d7d705d9
alghifarifikri <alghi77@gmail.com>
2023-08-11 20:26:20 +0700

    fix config for vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,8 +36,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  target: 'static',
-  router: {
-    base: '/business-platform-nuxt/', // Ganti dengan nama repositori Anda
-  },
+  // target: 'static',
+  // router: {
+  //   base: '/business-platform-nuxt/',
+  // },
 }
